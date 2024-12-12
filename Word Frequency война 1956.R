56b432cf84cce4decedbd3e39792959dd6573e8d
squig2019 <smith.12963@buckeyemail.osu.edu>
2024-12-11 19:15:28 -0500

    Word Frequency and Dispersion Plot - 1956
    CODE: Word Frequency and Dispersion Plot of the word война in the journal issues from 1956

---
+++ Word Frequency война 1956.R
@@ -0,0 +1,55 @@
+# Frequency of война in 1956
+
+rm(list = ls()) # Clear Workspace
+
+text_v <- scan("C:/Users/singi/Desktop/HSL/R Studio/Project/NEW_Texts/combined_text_1956.txt", what = "character", sep = "\n")
+length(text_v)
+novel_v <- paste(text_v, collapse = " ")
+length(novel_v)
+
+novel_lower_v <- tolower(novel_v)
+journal_word_1 <- strsplit(novel_lower_v, "\\W")
+
+str(journal_word_1)
+journal_word_v <- unlist(journal_word_1)
+not_blanks_v <- which(journal_word_v != " ")
+not_blanks_v[1:10]
+journal_word_v <- journal_word_v[not_blanks_v]
+journal_word_v
+not_blanks_v <- which(journal_word_v != "")
+not_blanks_v[1:10]
+journal_word_v <- journal_word_v[not_blanks_v]
+
+
+length(journal_word_v)
+length(unique(journal_word_v))
+sorted_journal_t <- table(journal_word_v)
+sorted_journal_t <- sort(sorted_journal_t, decreasing = TRUE)
+head(sorted_journal_t)
+top_hundred <- sorted_journal_t[1:100]
+top_twenty <- sorted_journal_t[1:20]
+top_twenty
+top_hundred
+
+
+i_time_v <- seq(from = 1, to = length(journal_word_v))
+война_v <- which(journal_word_v == "война")
+война_v
+в_count_v <- rep(NA, times = length(i_time_v))
+в_count_v[война_v] <- 1
+plot(
+  в_count_v,
+  main = "Dispersion Plot of 'война' in 1956",
+  xlab = "Issue Time",
+  ylab = "война",
+  type = "h",
+  ylim = c(0, 1), yaxt = 'n'
+)
+
+
+library(stringr)
+
+word_to_search <- "война"
+context_pattern <- paste0("([\\w]+(?:[\\s]+[\\w]+){0,2})?\\s*(война)\\s*([\\w]+(?:[\\s]+[\\w]+){0,2})?")
+matches <- str_match_all(novel_lower_v, context_pattern)
+str(matches)
